dfda8db523f74ec266082fcfd7810aa810550ec5
warscodev <warscodev@gmail.com>
2020-03-17 19:49:09 +0900

    CSS fin.

---
+++ app.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById("jsCanvas");
+
+function onMouseMove(event){
+    console.log(event)
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+}
+
+function init(){
+
+}
+
+
+init();
