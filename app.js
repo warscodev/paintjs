773af7ce0802888a2ce3cc694459b1e1e4beb54f
warscodev <warscodev@gmail.com>
2020-03-18 23:06:38 +0900

    Changing Color

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor")
 
 canvas.width = 700;
 canvas.height = 700;
@@ -30,10 +31,10 @@ function onMouseMove(event){
     }
 }
 
-function onMouseDown(event){
-    painting = true;
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
-
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
@@ -41,3 +42,5 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click",handleColorClick));
