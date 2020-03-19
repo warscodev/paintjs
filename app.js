7c022318eb842a380580d8b131498ac25d81a6fe
warscodev <warscodev@gmail.com>
2020-03-19 23:06:07 +0900

    집, 오후 11시 임시 커밋

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range  = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = 
 
 const INITIAL_COLOR = "2c2c2c";
 const CANVAS_SIZE = 700;
@@ -10,6 +11,9 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+//캔버스가 초기화 될 때 배경에 하얀색을 넣어준다.
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.55;
@@ -19,10 +23,12 @@ let filling = false;
 
 function stopPainting(){
     painting = false;
+    console.log("stopPainting!")
 }
 
 function startPainting(){
     painting = true;
+    console.log("startPainting!")
 }
 
 function onMouseMove(event){
@@ -30,10 +36,14 @@ function onMouseMove(event){
     const y = event.offsetY;
     if(!painting){
         ctx.beginPath();
+        console.log(`ctx.beginPath()`)
         ctx.moveTo(x, y);
+        //console.log(`ctx.moveTo(${x}, ${y})`)
     } else{
         ctx.lineTo(x, y);
+        console.log(`ctx.lineTo((${x}, ${y}))`)
         ctx.stroke();
+        console.log(`ctx.stroke((${x}, ${y}))`)
     }
 }
 
@@ -52,15 +62,20 @@ function handleModeClick(){
     if(filling==true){
         filling = false;
         mode.innerText = "Fill"
-    } else{
+    }else{
         filling = true;
         mode.innerText = "Paint";
     }
 }
 
 function handleCanvasClick(){
-    if(filling==true)
+    if(filling==true){
     ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+}
+
+function handleCM(event){
+    event.preventDefault();
 }
 
 if(canvas){
@@ -68,7 +83,9 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
-    canvas.addEventListener("click", handleCanvasClick)
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
+    canvas.
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click",handleColorClick));
