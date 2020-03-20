e9598761aeef7d7702db02f4094f4bd6e89a5443
warscodev <warscodev@gmail.com>
2020-03-20 17:48:03 +0900

    중간저장

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range  = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
-const saveBtn = 
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "2c2c2c";
 const CANVAS_SIZE = 700;
@@ -85,7 +85,6 @@ if(canvas){
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
     canvas.addEventListener("contextmenu", handleCM);
-    canvas.
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click",handleColorClick));
